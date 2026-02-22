011871a0fc70f189e5affec436478b3c96ed6fe2
zebenbrown <zeben.brown@mymail.champlain.edu>
2026-02-22 01:35:36 -0500

    tried to make progress on player arm dealing damage but didn't get anywhere

--- GS2/Assets/Scripts/PunchScript.cs
+++ GS2/Assets/Scripts/PunchScript.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PunchScript : Arm_Base
 {
     [SerializeField] private AnimationClip punchAnimationClip;
-
+    
     private string punchAnimationClipName;
     private Animator animator;
     private float animationTimer = 0;
@@ -58,53 +58,50 @@ public class PunchScript : Arm_Base
         PunchForward();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
-        /*if (collision.gameObject.CompareTag("ComboEnemy"))
+        Debug.Log("Collision with: " + collision.gameObject.name);
+        
+        if (GetComponentInParent<PlayerController>() == null)
         {
-            ComboEnemy.takeDamage(34);
+            return;
         }
-
-        if (collision.gameObject.CompareTag("RangedEnemy"))
-        {
-            RangedEnemy.takeDamage(34);
-        }
-
-        if (collision.gameObject.CompareTag("MeleeEnemy"))
-        {
-            MeleeEnemy.takeDamage(34);
-        }*/
-
-
-        /*if (collision.gameObject.CompareTag("MeleeEnemy"))
-        {
-            meleeEnemy.GetComponent<MeleeEnemy>();
-            meleeEnemy.takeDamage(34);
-            Destroy(gameObject);
-        }
-
-
-        if (collision.gameObject.CompareTag("RangedEnemy"))
-        {
-            rangedEnemy.GetComponent<RangedEnemy>();
-            rangedEnemy.takeDamage(34);
-            Destroy(gameObject);
-        }
-
-
-        if (collision.gameObject.CompareTag("ComboEnemy"))
-        {
-            comboEnemy.GetComponent<ComboEnemy>();
-            comboEnemy.takeDamage(34);
-            Destroy(gameObject);
-        }*/
 
         Enemy enemy = collision.gameObject.GetComponent<Enemy>();
         if (enemy != null)
         {
             enemy.takeDamage(34);
         }
-        
+
+        /*if (transform.parent.parent.CompareTag("Player"))
+        {
+            if (collision.gameObject.CompareTag("MeleeEnemy") || collision.gameObject.CompareTag("RangedEnemy") ||
+                collision.gameObject.CompareTag("ComboEnemy"))
+            {
+                Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+                if (enemy != null)
+                    enemy.takeDamage(34);
+            }
+        }
+
+        else
+        {
+            return;
+        }#1#
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("EnemyArm"))
+        {
+            return;
+        }
+       
+        Enemy enemy = other.GetComponentInParent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.takeDamage(34);
+            Debug.Log("Hit enemy");
+        }
     }
-    
 }
