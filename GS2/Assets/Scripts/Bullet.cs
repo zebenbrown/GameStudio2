81d4efce8468305f2110b4d4efe0a1ec68bc738d
Morgan Walls <morgan.walls@mymail.champlain.edu>
2026-02-22 11:05:36 -0500

    Enemies shoot and do damage to the player with bullets

--- GS2/Assets/Scripts/Bullet.cs
+++ GS2/Assets/Scripts/Bullet.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private MeleeEnemy meleeEnemy;
+    [SerializeField] [Range(0f, 1f)] private float alpha = 1f;
+    [SerializeField] private Material bulletMaterial;
+    private Color color;
 
-    [SerializeField] private RangedEnemy rangedEnemy;
+    private Animator animator;
+    [SerializeField] private AnimationClip destroyClip;
 
-    [SerializeField] private ComboEnemy comboEnemy;
+    //[SerializeField] private MeleeEnemy meleeEnemy;
+    //[SerializeField] private RangedEnemy rangedEnemy;
+    //[SerializeField] private ComboEnemy comboEnemy;
+
+    [SerializeField] private const float BULLET_DAMAGE = 20.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        color = bulletMaterial.color;
+        animator = GetComponent<Animator>();
+        
+
         Destroy(gameObject, 5.0f);
+    }
+
+    private void Update()
+    {
+        //updateTransparency();
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"Hit: {collision.gameObject.name}, Layer: {LayerMask.LayerToName(collision.gameObject.layer)}");
+
         /*if (collision.gameObject.CompareTag("Player"))
         {
             PlayerController.takeDamage(20);
@@ -46,11 +62,28 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }*/
 
-        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-        if (enemy != null)
+        if (collision.gameObject.TryGetComponent<Enemy>(out Enemy enemy))
         {
-            enemy.takeDamage(20);
-            Destroy(gameObject);
+            //Debug.Log($"Hit: {collision.gameObject.name}, Layer: {LayerMask.LayerToName(collision.gameObject.layer)}");
+
+            enemy.takeDamage(BULLET_DAMAGE);
         }
+        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            //Debug.LogWarning($"Hit: {collision.gameObject.name}, Layer: {LayerMask.LayerToName(collision.gameObject.layer)}");
+
+            player.takeDamage(BULLET_DAMAGE);
+        }
+
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        animator.Play(destroyClip.name);
+        //Destroy(gameObject);
+    }
+
+    private void updateTransparency()
+    {
+        color.a = alpha;
+
+        bulletMaterial.color = color;
     }
 }
--- GS2/Assets/Scripts/PunchScript.cs
+++ GS2/Assets/Scripts/PunchScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 public class PunchScript : Arm_Base
@@ -9,23 +10,21 @@ public class PunchScript : Arm_Base
     private Animator animator;
     private float animationTimer = 0;
 
-    private AudioSource audioSource;
+    [SerializeField] private AudioClip punchAudio;
 
-    protected override void ArmSpecificStart()
+    protected override void armSpecificStart()
     {
         animator = GetComponent<Animator>();
         punchAnimationClipName = punchAnimationClip.name;
         animator.enabled = false;
-
-        audioSource = GetComponent<AudioSource>();
     }
 
-    protected override void SpecificEquip()
+    protected override void specificEquip()
     {
         animator.enabled = true;
     }
 
-    protected override void SpecificDrop()
+    protected override void specificDrop()
     {
         animator.enabled = false;
     }
@@ -46,14 +45,10 @@ public class PunchScript : Arm_Base
     {
         animator.Play(punchAnimationClipName);
 
-        if (!audioSource.isPlaying && animationTimer == 0)
-        {
-            audioSource.Play();
-            animationTimer = punchAnimationClip.length;
-        }
+        playActivateSound();
     }
 
-    public override void ArmMainAction()
+    public override void armMainAction()
     {
         PunchForward();
     }
@@ -99,12 +94,32 @@ public class PunchScript : Arm_Base
             Destroy(gameObject);
         }*/
 
-        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-        if (enemy != null)
+        if (!isEnemyArm)
         {
-            enemy.takeDamage(34);
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.takeDamage(34);
+            }
+        }
+        else
+        {
+            /*
+            if (collision.gameObject == gameManager.getPlayer().gameObject)
+            {
+
+            }
+            */
         }
         
     }
-    
+
+    protected override void playActivateSound()
+    {
+        if (animationTimer == 0)
+        {
+            audioSource.Play();
+            animationTimer = punchAnimationClip.length;
+        }
+    }
 }
